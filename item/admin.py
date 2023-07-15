b7cb8e77911bce57f8fc014d0c796cd05d1d8dcc
Hua Szu Yang <33264733+huaszu@users.noreply.github.com>
2023-07-15 17:57:34 -0400

    Create Category model representing category of items

--- item/admin.py
+++ item/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Category
+
+# To get database table to show in Django administration interface
+admin.site.register(Category) 
