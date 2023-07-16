ebd2621867c372cfc47a82134d72c36afb8de1f8
Hua Szu Yang <33264733+huaszu@users.noreply.github.com>
2023-07-16 13:39:47 -0400

    Add detail page for each item, which allows browsing of related items - up to three other items in the same category.  Enhance front page with categories and count of items in each category.

--- item/views.py
+++ item/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
-# Create your views here.
+from .models import Item
+
+def detail(request, pk):
+    # 404 if object does not exist in db
+    # `pk=pk`: First pk is primary key in db.  Second pk is from URL
+    item = get_object_or_404(Item, pk=pk)
+    related_items = Item.objects.filter(category=item.category, is_sold=False).exclude(pk=pk)[0:3]
+
+    return render(request, 'item/detail.html', {
+        'item': item,
+        'related_items': related_items
+    })
