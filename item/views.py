be6ae6a600ab287bba69eecd8f6b081e48111f60
Hua Szu Yang <33264733+huaszu@users.noreply.github.com>
2023-07-16 15:35:08 -0400

    Allow user to edit their items.

--- item/views.py
+++ item/views.py
@@ -2,7 +2,7 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404, redirect
 
 from .models import Item
-from .forms import NewItemForm
+from .forms import NewItemForm, EditItemForm
 
 def detail(request, pk):
     # 404 if object does not exist in db
@@ -37,6 +37,26 @@ def new(request):
     return render(request, 'item/form.html', {
         'form': form,
         'title': 'New item', 
+    })
+
+@login_required
+def edit(request, pk):
+    item = get_object_or_404(Item, pk=pk, created_by=request.user)
+
+    if request.method == 'POST':
+        form = EditItemForm(request.POST, request.FILES, instance=item)
+
+        if form.is_valid():
+            form.save()
+
+            return redirect('item:detail', pk=item.id)
+    else: # If request is GET request
+        # form = EditItemForm() # Form would be blank
+        form = EditItemForm(instance=item)
+
+    return render(request, 'item/form.html', {
+        'form': form,
+        'title': 'Edit item', 
     })
 
 @login_required
